c6fea9a68ea353496c21dd7c19c95af2905535e0
Amrutha Prakash Veena <pvamrutha2509@gmail.com>
2021-01-19 10:48:46 -0500

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,10 @@
+#Time: O(N)
+#Space: O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = nums[0]
+
+        for i in range(1, len(nums)):
+            result = result ^ nums[i]
+
+        return result
