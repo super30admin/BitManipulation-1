451f25e8b77500759d1d4aa39d568ca6791041e5
Yasunori Iwayanagi <yiwayanagi@upward.net>
2021-04-02 10:19:05 -0700

    Submission BitManipulation-1

---
+++ PairOfSingleNumber.java
@@ -0,0 +1,23 @@
+//Time Complexity O(n)
+//Space Complexity O(1)
+
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int x=0;
+        for(int num:nums)
+        {
+            x=x^num;
+        }
+        int rightmost=x&(-x);
+        int output=0;
+        for(int num:nums)
+        {
+            if((rightmost&num)!=0)
+            {
+                output=output^num;
+            }
+        }
+        return new int[]{output,x^output};
+    }
+} 
