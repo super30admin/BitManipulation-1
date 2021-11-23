9e8214e21723725dac2c3b61d28c667b0863f3ee
Tanvie Kirane <67563504+tanviekirane@users.noreply.github.com>
2021-11-23 00:21:17 -0500

    created singleNumber.java

---
+++ singleNumber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int bitmask = 0;
+        for(int num: nums){
+            bitmask = num ^ bitmask;
+        }
+        return bitmask;
+    }
+}
