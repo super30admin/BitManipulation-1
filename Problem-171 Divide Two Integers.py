a59d31442b59e2b4ce96e7ce489414791f5567ed
Adhish Shrivastava <adhish70@gmail.com>
2022-01-18 09:53:18 -0500

    Problem 172 done

---
+++ Problem-171 Divide Two Integers.py
@@ -0,0 +1,11 @@
+# 29. Divide Two Integers
+# https://leetcode.com/problems/divide-two-integers/
+
+# Logic: 
+
+# Time complexity: 
+# Space Complexity: 
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        pass
