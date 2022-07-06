947b2c394279e560a657ae33fd5d526b46276542
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-07-05 23:56:37 -0700

    Add files via upload

---
+++ SingleNumberIII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulation
+{
+    class SingleNumberIII
+    {
+        /*
+         * T.C: O(n) since interating through all the nums 
+         * S.C: O(1)
+         */
+        public int[] SingleNumber(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return new int[] { };
+
+            int xor = 0;
+
+            foreach (int num in nums)
+            {
+                xor = xor ^ num;
+            }
+
+            int xor2 = 0;
+            int temp = xor & (-xor);
+
+            foreach (int num in nums)
+            {
+                if ((temp & num) != 0)
+                {
+                    xor2 = xor2 ^ num;
+                }
+            }
+
+            return new int[] { xor2, xor ^ xor2 };
+        }
+    }
+}
