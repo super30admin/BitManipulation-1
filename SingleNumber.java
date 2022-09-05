fdbfe30615f0a472e3456b045361e33301c8287a
saurabhgujare <gujare.s@northeastern.edu>
2022-09-05 19:49:53 -0400

    Completed Bit Manipulation 1

--- SingleNumber.java
+++ SingleNumber.java
@@ -4,23 +4,10 @@
 // Any problem you faced while coding this : No
 public class SingleNumber {
     class Solution {
-        public int[] singleNumber(int[] nums) {
-            int bitmask = 0;
-
-            for(int i=0; i<nums.length; i++) {
-                bitmask ^= nums[i]; // 1st eq x1 + y1 = c1
-            }
-
-            int temp = bitmask & - bitmask; // bitmask & (2's compliment(bitmask))
-
-            int bitmask2 = 0;
-            for(int i=0; i<nums.length; i++) {
-                if((nums[i] & temp) != 0) {
-                    bitmask2 ^= nums[i]; // 2nd eq x1 - y1 = c2
-                }
-
-            }
-            return new int[] {bitmask2, bitmask2 ^ bitmask};
+        public List<String> findRepeatedDnaSequences(String s) {
+            int x=0;
+            for(int i : nums)x^=i;
+            return x;
         }
     }
 }
